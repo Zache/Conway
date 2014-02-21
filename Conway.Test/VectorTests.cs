504ca29e050eb04e3197ee94f0cc9598c8864f61
Zacharias Björngren <z@zmb.se>
2014-02-21 11:39:21 +0100

    Tests of the basic vector

---
+++ Conway.Test/VectorTests.cs
@@ -0,0 +1,47 @@
+﻿namespace Conway.Test
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Security.Policy;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Game;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class VectorTests
+    {
+        [Test]
+        public void Vector_Has_8_Neighbours()
+        {
+            var v = new Vector(0, 0);
+
+            var neighbours = v.Neighbours();
+
+            Assert.AreEqual(8, neighbours.Count(), "Wrong number of neighbours");
+        }
+
+        [Test]
+        public void Vector_0_0_Has_Correct_Neighbours()
+        {
+            var v = new Vector(0, 0);
+
+            var neighbours = v.Neighbours();
+
+            CollectionAssert.AreEquivalent(
+                new[]
+                {
+                    new Vector(0,1),
+                    new Vector(1,1),
+                    new Vector(1,0),
+                    new Vector(-1,0),
+                    new Vector(-1,-1),
+                    new Vector(0,-1),
+                    new Vector(-1,1),
+                    new Vector(1,-1)
+                }
+                , neighbours, "Wrong number of neighbours");
+        }
+    }
+}
